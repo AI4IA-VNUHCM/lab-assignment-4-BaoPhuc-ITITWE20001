2bc50d05cc6816d311c4dcfb4dfdee9b3b5f6344
BaoPhuc-ITITWE20001 <ITITWE20001@student.hcmiu.edu.vn>
2021-10-25 19:28:57 +0700

    update

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -17,21 +17,20 @@ ______________________________________
 
 void Ex1(int n){
 	//Your codes here
-      int arr[n][n];
-
-    for (int line = 0; line < n; line++)
-    {
-        for (int i = 0; i <= line; i++)
-        {
-        if (line == i || i == 0)
-            arr[line][i] = 1;
-        else
-            arr[line][i] = arr[line - 1][i - 1] +
-                            arr[line - 1][i];
-        printf("%d", arr[line][i]);
-        }
-        printf("\n");
-    }
+int arr[10][10];
+for (int i = 0; i <= n; i++)
+{ 
+	for (int j = 0; j <= i; j++)
+	{ if (i == j || j == 0) 
+	{ arr[i][j] = 1;
+	} 
+	else 
+	{ arr[i][j]= arr[i-1][j-1]+ arr[i-1][j];
+	}
+	printf("%d ", arr[i][j]);
+	}
+	printf("\n");
+}
 }
 
 int main(int argc, char *argv[]) {
